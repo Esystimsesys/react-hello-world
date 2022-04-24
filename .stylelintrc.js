589e84707dad0dc15e9fc8b28748bc3d6303ce2a
Wataru Nawata <nwataru1223@gmail.com>
2022-04-24 17:25:31 +0900

    first commit

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  plugins: ['stylelint-order'],
+  ignoreFiles: ['**/node_modules/**'],
+  rules: {
+    'string-quotes': 'single',
+    'selector-class-pattern': null,
+    'keyframes-name-pattern': null,
+  },
+};
